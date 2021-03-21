7b5cfa0a2326d0ed6a13c73450570eb0f60e304f
gebageba <nagaikanae34@gmail.com>
2021-03-21 15:10:40 +0800

    update

--- api.js
+++ api.js
@@ -1,6 +1,16 @@
 window.addEventListener('load', () => {
+  
+  
+  const octokit = new Octokit({ auth: `personal-access-token123` });
+
   const n = document.getElementById('newlist');
   const v = document.getElementById('lists');
+
+  const result = await octokit.request('GET /orgs/{org}/repos', {
+    org: 'org'
+  })
+
+ console.log(result);
 
   n.addEventListener("click", makeList)
   const text = document.getElementById('text');
--- index.js
+++ index.js
@@ -2,14 +2,15 @@ window.addEventListener('load', () => {
   const f = document.getElementById('time');
   const s = document.getElementById('second');
   const h = document.getElementById('hour');
-  const stop = document.getElementById('stop');
+  const stop = document.getElementById('stop2');
   let timerS;
   let cntSecond = 1499;
+  var d = "集中しましょう";
 
   f.addEventListener("click", startFunction)
 
   function startFunction() {
-    f.innerHTML = "集中してください";
+    f.innerHTML = d;
     timerS = setInterval(countDown,1000);
     
     function countDown() {
@@ -21,15 +22,23 @@ window.addEventListener('load', () => {
         sound("sine", 10);
       }
     }
-    
+
+  stop.addEventListener("click",stopFunction)
+
+  function stopFunction() {
+    console.log("ok");
+    countDown.terminate();
+    countDown = undefined;
+  };
+
     function sound(type, sec) {
       var AudioContext = window.AudioContext || window.webkitAudioContext,
       ctx = new AudioContext();
       var oscillator = ctx.createOscillator();
       var gainNode = ctx.createGain();
       oscillator.detune.value = 0; // 音の高さとか調整？
-      oscillator.frequency.value = 174; // 周波数 880Hz
-      gainNode.gain.value = 0.05; // 音量
+      oscillator.frequency.value = 700; // 周波数 880Hz
+      gainNode.gain.value = 0.1; // 音量
       
       oscillator.connect(gainNode);
       gainNode.connect(ctx.destination);
